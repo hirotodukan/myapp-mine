29ca80b8114854b42ad8fa0d24141c6479bc8ff5
hirotodukan <57314296+hirotodukan@users.noreply.github.com>
2019-11-28 19:51:08 +0900

    pass

--- db/migrate/20191128050058_create_reviews.rb
+++ db/migrate/20191128050058_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-
+      t.string :title
+      t.string :genre
+      t.string :comment
+      t.text :image
       t.timestamps
     end
   end
